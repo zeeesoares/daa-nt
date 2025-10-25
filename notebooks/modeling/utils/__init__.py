b3208642c7832b41c105af1fd859defc9cadd1f3
zeeesoares <zecostasoares04@gmail.com>
2025-10-25 17:21:45 +0100

    chore: removed datautils due to inconsistency in read_csv func

--- notebooks/modeling/utils/__init__.py
+++ notebooks/modeling/utils/__init__.py
@@ -1,12 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+OUTPUT_DIR = "../../submissions"
 
-TRAIN_PATH = os.getenv("TRAIN_PATH")
-TEST_PATH = os.getenv("TEST_PATH")
-OUTPUT_DIR = os.getenv("OUTPUT_DIR", "../submissions")
-
-print(f"[utils] TRAIN_PATH={TRAIN_PATH}")
-print(f"[utils] TEST_PATH={TEST_PATH}")
 print(f"[utils] OUTPUT_DIR={OUTPUT_DIR}")
--- notebooks/modeling/utils/data_utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from . import TRAIN_PATH, TEST_PATH
-
-def load_datasets():
-    train = pd.read_csv(TRAIN_PATH, keep_default_na=False)
-    test = pd.read_csv(TEST_PATH, keep_default_na=False)
-    return train, test
